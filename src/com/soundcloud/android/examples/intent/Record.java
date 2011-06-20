b47567d8a4e206663929d674dca813b450db2bd0
Jan Berkel <jan.berkel@gmail.com>
2011-06-20 12:13:33 +0200

    Initial commit

---
+++ src/com/soundcloud/android/examples/intent/Record.java
@@ -0,0 +1,14 @@
+package com.soundcloud.android.examples.intent;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Record extends Activity
+{
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.record);
+    }
+}
